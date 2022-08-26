def4bbc37653e124da4aa6d280e59ae390f6fa53
vaishakhs <111145609+vaishakhs@users.noreply.github.com>
2022-08-26 22:37:01 +0530

    adding a.sh

---
+++ a.sh
@@ -0,0 +1,3 @@
+echo "How to connect to your instance, Your instance is launching and it might be a few minutes until it is in the running state, 
+when it will be ready for you to use
+Click View Instances to monitor your instance's status. Once your instance is in the 'running' state, you can connect to it from the Instances screen. Find out how to connect to your instance "
